68c3065ac8296adbee96fc484210057e869e24f5
MatteoLarrode <matteo.larrode@orange.fr>
2023-05-25 01:08:07 +0200

    used use_pipe() for dependency

--- R/filter_delivered.R
+++ R/filter_delivered.R
@@ -1,11 +1,18 @@
-#' Filter delivered kits
+#' @title Filter delivered kits
 #'
-#' @param df A dataframe of Equipage Solidaire orders with a "tracking_status" variable
+#' @description This function filters only the orders that have been delivered, and removes from a dataframe the orders that have been cancelled.
+#'     It does so by using the "tracking_status" variable that is present in Delivraide data.
+#'
+#' @param df A dataframe of Delivraide orders
 #'
 #' @return A dataframe
+#'
+#' @importFrom dplyr filter
+#'
 #' @export
 #'
 #' @examples
 filter_delivered <- function(df) {
-  df %>% filter(tracking_status == 3)
+  df %>%
+    dplyr::filter(tracking_status == 3)
 }
