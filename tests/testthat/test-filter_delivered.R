35a64d7758b1f65b4778637ff499b01697ff9831
MatteoLarrode <matteo.larrode@orange.fr>
2023-05-25 14:42:43 +0200

    created test for filter_delivered

---
+++ tests/testthat/test-filter_delivered.R
@@ -0,0 +1,19 @@
+test_that("filter_delivered returns expected results", {
+  # Create a sample dataframe for testing
+  df <- data.frame(
+    tracking_status = c(1, 2, 3, 3, 1, 3, 2),
+    other_column = c("A", "B", "C", "D", "E", "F", "G")
+  )
+
+  # Call the function under test
+  filtered_df <- filter_delivered(df)
+
+  # Check if the returned object is a dataframe
+  expect_true(is.data.frame(filtered_df), info = "Returned object is not a dataframe.")
+
+  # Check if the returned dataframe only contains rows with tracking_status equal to 3
+  expect_equal(filtered_df$tracking_status, c(3, 3, 3), info = "Incorrect filtering of tracking_status.")
+
+  # Check if the other_column remains intact
+  expect_equal(filtered_df$other_column, c("C", "D", "F"), info = "Incorrect filtering of other_column.")
+})
