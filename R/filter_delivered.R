b88974ae9f79d43ec19fea2faabddb8171b73787
MatteoLarrode <matteo.larrode@orange.fr>
2023-05-27 19:04:14 +0200

    added example in filter_delivered() & added variable for tracking column

--- R/filter_delivered.R
+++ R/filter_delivered.R
@@ -1,9 +1,10 @@
 #' @title Filter delivered kits
 #'
 #' @description This function filters from the dataset the orders that have been delivered, and removes those that have been cancelled.
-#'  It does so by using the "tracking_status" variable that is present in Delivraide data.
+#'   It does so by using the specified tracking variable, defaulting to "tracking_status", that is present in Delivraide data.
 #'
 #' @param df A dataframe of Delivraide orders
+#' @param col_name The column to use for filtering (default: tracking_status). Note: do not add quotes around the column name.
 #'
 #' @return A dataframe
 #'
@@ -12,10 +13,21 @@
 #' @export
 #'
 #' @examples
-filter_delivered <- function(df) {
+#' # Load the sample orders data
+#' data(sample_orders_data)
+#'
+#' # Filter the delivered orders
+#' delivered_orders <- filter_delivered(sample_orders_data)
+#'
+#' # Print the filtered dataframe
+#' print(delivered_orders)
+#'
+filter_delivered <- function(df, col_name = tracking_status) {
 
   stopifnot("Input must be a dataframe" = is.data.frame(df))
 
+  stopifnot("col_name must be an unquoted column name" = !is.character(substitute(col_name)))
+
   df %>%
-    dplyr::filter(tracking_status == 3)
+    dplyr::filter({{col_name}} == 3)
 }
--- tests/testthat/test-filter_delivered.R
+++ tests/testthat/test-filter_delivered.R
@@ -17,3 +17,34 @@ test_that("filter_delivered returns expected results", {
   # Check if the other_column remains intact
   expect_equal(filtered_df$other_column, c("C", "D", "F"), info = "Incorrect filtering of other_column.")
 })
+
+
+test_that("filter_delivered handles different column names", {
+  # Create a sample dataframe for testing
+  df <- data.frame(
+    custom_status = c(1, 2, 3, 3, 1, 3, 2),
+    other_column = c("A", "B", "C", "D", "E", "F", "G")
+  )
+
+  # Call the function under test with custom column name
+  filtered_df <- filter_delivered(df, col_name = custom_status)
+
+  # Check if the returned dataframe only contains rows with custom_status equal to 3
+  expect_equal(filtered_df$custom_status, c(3, 3, 3), info = "Incorrect filtering of custom_status.")
+
+  # Check if the other_column remains intact
+  expect_equal(filtered_df$other_column, c("C", "D", "F"), info = "Incorrect filtering of other_column.")
+})
+
+
+test_that("filter_delivered throws an error for invalid col_name input", {
+  # Create a sample dataframe for testing
+  df <- data.frame(
+    tracking_status = c(1, 2, 3, 3, 1, 3, 2),
+    other_column = c("A", "B", "C", "D", "E", "F", "G")
+  )
+
+  # Check if an error is thrown when col_name is a character string
+  expect_error(filter_delivered(df, col_name = "tracking_status"),
+               info = "An error should be thrown for col_name as a string.")
+})
